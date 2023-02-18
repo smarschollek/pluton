f5b34f02899d79c5c1827c4eba888391db351f85
Stephan Marschollek <stephan.marschollek@gmail.com>
2023-02-18 20:40:21 +0100

    adds tailwindcss

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,49 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div
+      className="place-items-center grid h-screen bg-gradient-to-t to-black from-purple-900"
+    >
+      <div
+        className="bg-white bg-opacity-20 p-4 rounded shadow-2xl text-white"
+      >
+        <h1
+          className="text-3xl font-bold underline mb-5"
+        >
+          Welcome to Remix 13
+        </h1>
+        <ul>
+          <li>
+            <a
+              className="hover:text-green-500"
+              href="https://remix.run/tutorials/blog"
+              rel="noreferrer"
+              target="_blank"
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              className="hover:text-green-500"
+              href="https://remix.run/tutorials/jokes"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              className="hover:text-green-500"
+              href="https://remix.run/docs"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
